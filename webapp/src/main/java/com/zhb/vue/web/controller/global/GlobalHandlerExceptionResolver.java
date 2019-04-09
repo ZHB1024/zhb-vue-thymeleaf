1c69f3175d69043f7fea606589e1a5f8e5d600dd
ZHB-JOB <zhb20111503@126.com>
2019-04-09 16:23:59 +0800

    全局异常处理

--- webapp/src/main/java/com/zhb/vue/web/controller/global/GlobalHandlerExceptionResolver.java
+++ webapp/src/main/java/com/zhb/vue/web/controller/global/GlobalHandlerExceptionResolver.java
@@ -1,11 +1,7 @@
 package com.zhb.vue.web.controller.global;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.alibaba.fastjson.support.config.FastJsonConfig;
-import com.alibaba.fastjson.support.spring.FastJsonJsonView;
 import com.zhb.forever.framework.exception.ExceptionUtil;
-import com.zhb.forever.framework.util.AjaxData;
+import com.zhb.vue.Constant;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,26 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.ConversionNotSupportedException;
-import org.springframework.beans.TypeMismatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.stereotype.Component;
-import org.springframework.validation.BindException;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingPathVariableException;
-import org.springframework.web.bind.MissingServletRequestParameterException;
-import org.springframework.web.bind.ServletRequestBindingException;
-import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
-import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 /**
 
@@ -56,10 +37,9 @@ public class GlobalHandlerExceptionResolver implements HandlerExceptionResolver,
     public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler,
             Exception exception) {
         
-        String exceptionMessage = ExceptionUtil.getExceptionMessage(exception);
-        logger.error(exceptionMessage);
+        logger.error(exception.getMessage());
         Map<String, Object> model = new HashMap<String, Object>();
-        model.put("errorMessage", exception.getMessage());
+        model.put(Constant.REQUEST_ERROR, exception.getMessage());
         return new ModelAndView("error/index", model);
     }
     
